0956145475b08cc7ed02fa82c0b51c92eccf6e86
bianloic <bian.loic@gmail.com>
2014-10-06 16:08:08 +0200

    add method deviceinsuranceByModele

--- src/fr/adaming/awal/controller/DeviceinsurancemodelController.java
+++ src/fr/adaming/awal/controller/DeviceinsurancemodelController.java
@@ -8,6 +8,8 @@ package fr.adaming.awal.controller;
 import fr.adaming.awal.controller.interfaces.IDeviceinsurancemodelController;
 import fr.adaming.awal.dao.interfaces.IDeviceInsuranceModeleDao;
 import fr.adaming.awal.entity.Deviceinsurancemodel;
+import fr.adaming.awal.entity.Modele;
+import java.util.List;
 
 /**
  *
@@ -19,5 +21,10 @@ implements IDeviceinsurancemodelController{
     
     public DeviceinsurancemodelController(){
         super(Deviceinsurancemodel.class);
+    }
+
+    @Override
+    public List<Deviceinsurancemodel> getDeviceInsuranceModelsByModel(Modele modele) {
+        return dao.getDeviceInsuranceModelsByModel(modele);
     }
 }
