2c485af76da369323a15086b543bd4513a71d2fd
Antoine <git.alucas@gmail.com>
2014-10-10 16:45:26 +0200

    Move actions from view to controller

--- src/fr/adaming/awal/controller/DeviceRepairController.java
+++ src/fr/adaming/awal/controller/DeviceRepairController.java
@@ -6,11 +6,21 @@
 package fr.adaming.awal.controller;
 
 import fr.adaming.awal.controller.interfaces.IDeviceRepairController;
+import fr.adaming.awal.dao.interfaces.IDeviceInsuranceDao;
 import fr.adaming.awal.dao.interfaces.IDeviceRepairDao;
+import fr.adaming.awal.dao.interfaces.IRepairerDao;
 import fr.adaming.awal.entity.Client;
 import fr.adaming.awal.entity.Device;
+import fr.adaming.awal.entity.Deviceinsurance;
 import fr.adaming.awal.entity.Devicerepair;
 import fr.adaming.awal.entity.Modelpackage;
+import fr.adaming.awal.entity.Repairer;
+import fr.adaming.awal.util.DeviceRepairerUtil;
+import fr.adaming.awal.util.RepairerUtil;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,13 +29,90 @@ import java.util.List;
  */
 public class DeviceRepairController extends Controller<IDeviceRepairDao, Devicerepair, Integer> implements IDeviceRepairController {
 
+    private IRepairerDao repairerDao;
+    private IDeviceInsuranceDao deviceInsuranceDao;
+
     @Override
     public List<Devicerepair> getDevicesRepairByClient(final Client client) {
         return dao.getDevicesRepairByClient(client);
     }
 
     @Override
-    public Devicerepair getByDeviceAndPackage(final Device device, final Modelpackage modelPackage) {
-        return dao.getByDeviceAndPackage(device, modelPackage);
+    public boolean create(Devicerepair t) {
+        throw new RuntimeException("Don't use that method, use createDeviceRepair instead");
     }
+
+    /**
+     *
+     * @param entity
+     * @throws PackageAlreadyPresentException
+     */
+    @Override
+    public void createDeviceRepair(final Devicerepair entity) throws IDeviceRepairController.PackageAlreadyPresentException {
+        Devicerepair devicerepair = dao.getByDeviceAndPackage(entity.getDevice(), entity.getModelpackage());
+        if (devicerepair != null) {
+            throw new IDeviceRepairController.PackageAlreadyPresentException();
+        }
+
+        Repairer repairer = null;
+        for (Repairer repairerTmp : repairerDao.getAll()) {
+            if (repairerTmp.getAvailable().equals(RepairerUtil.AVAILABLE)) {
+                if (repairerTmp.getFirm().getAddress().getPostcode().equals(entity.getDevice().getClient().getAddress().getPostcode())) {
+                    repairer = repairerTmp;
+                }
+            }
+        }
+
+        if (deviceAsInsurance(entity.getDevice(), entity.getDevice().getClient()) == true) {
+            entity.setPrice(0);
+        } else {
+            entity.setPrice(Integer.valueOf(entity.getModelpackage().getPrice()));
+        }
+
+        entity.setRepairer(repairer);
+        entity.setState(DeviceRepairerUtil.STATE_CREATE);
+        entity.setDateCreation(Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant()));
+
+        super.create(entity);
+    }
+
+    private boolean deviceAsInsurance(final Device device, final Client client) {
+        boolean insurance = false;
+
+        for (Deviceinsurance deviceinsurance : deviceInsuranceDao.getDevicesInsuranceByClient(client)) {
+            if (deviceinsurance.getDevice().equals(device)) {
+                Calendar calLastTime = Calendar.getInstance();
+                calLastTime.setTime(deviceinsurance.getBeginDate());
+                int duration = deviceinsurance.getDeviceinsurancemodel().getDuration();
+                calLastTime.add(Calendar.MONTH, duration);
+
+                System.out.println(" calendar add duration : " + (calLastTime.get(Calendar.MONTH) + 1) + "-" + calLastTime.get(Calendar.DATE) + "-" + calLastTime.get(Calendar.YEAR));
+                Calendar calTime = Calendar.getInstance();
+
+                if (calTime.compareTo(calLastTime) <= 0) {
+                    insurance = true;
+                }
+            }
+        }
+
+        System.out.println("insurance : " + insurance);
+        return insurance;
+    }
+
+    public IRepairerDao getRepairerDao() {
+        return repairerDao;
+    }
+
+    public void setRepairerDao(IRepairerDao repairerDao) {
+        this.repairerDao = repairerDao;
+    }
+
+    public IDeviceInsuranceDao getDeviceInsuranceDao() {
+        return deviceInsuranceDao;
+    }
+
+    public void setDeviceInsuranceDao(IDeviceInsuranceDao deviceInsuranceDao) {
+        this.deviceInsuranceDao = deviceInsuranceDao;
+    }
+
 }
